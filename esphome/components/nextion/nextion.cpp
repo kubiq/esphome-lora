d3fc76a67a9ae00936f322ef71c7bfa56c566bd2
Senex Crenshaw <senexcrenshaw@gmail.com>
2021-02-23 05:19:39 +0000

    getHeapSize fixup

--- esphome/components/nextion/nextion.cpp
+++ esphome/components/nextion/nextion.cpp
@@ -168,7 +168,7 @@ void Nextion::loop() {
 // nextion.tech/instruction-set/
 bool Nextion::process_nextion_commands_() {
   if (this->is_test_debug()) {
-    ESP_LOGW(TAG, "LOOP sleep : %s", this->is_sleeping() ? "True" : "False");
+    ESP_LOGD(TAG, "LOOP Nextion is sleep : %s", this->is_sleeping() ? "True" : "False");
     ESP_LOGD("*** HEAP", " Free: %d", ESP.getFreeHeap());
 #ifdef ARDUINO_ARCH_ESP32
     ESP_LOGD("*** HEAP", "Total: %d", ESP.getHeapSize());
